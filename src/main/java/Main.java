29aa8a65062a80f93914a9b7ee239cae81ab95b0
halseyec <curry.halsey@gmail.com>
2023-10-19 16:31:10 -0600

    Fix main logic

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,33 +4,42 @@ public class Main {
 
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
-    String input = scanner.nextLine();
-    String[] parts = input.split(" ");
 
-    Calculator calculator = new Calculator();
+    System.out.println("Welcome to the calculator.");
 
-    switch (parts[0].toLowerCase()) {
-      case "add":
-        System.out.println(calculator.add(Integer.parseInt(parts[1]), Integer.parseInt(parts[2])));
-        break;
-      case "subtract":
-        System.out.println(calculator.subtract(Integer.parseInt(parts[1]), Integer.parseInt(parts[2])));
-        break;
-      case "multiply":
-        System.out.println(calculator.multiply(Integer.parseInt(parts[1]), Integer.parseInt(parts[2])));
-        break;
-      case "divide":
-        System.out.println(calculator.divide(Integer.parseInt(parts[1]), Integer.parseInt(parts[2])));
-        break;
-      case "fibonacci":
-        System.out.println(calculator.fibonacciNumberFinder(Integer.parseInt(parts[1])));
-        break;
-      case "binary":
-        System.out.println(calculator.intToBinaryNumber(Integer.parseInt(parts[1])));
-        break;
-      default:
-        System.out.println("Invalid command!");
-        break;
+    while (true) {
+      System.out.println("Enter a command:");
+      String input = scanner.nextLine();
+      if (input.equalsIgnoreCase("exit")) {
+        break; // Exits the loop and ends the program
+      }
+      String[] parts = input.split(" ");
+
+      Calculator calculator = new Calculator();
+
+      switch (parts[0].toLowerCase()) {
+        case "add":
+          System.out.println(calculator.add(Integer.parseInt(parts[1]), Integer.parseInt(parts[2])));
+          break;
+        case "subtract":
+          System.out.println(calculator.subtract(Integer.parseInt(parts[1]), Integer.parseInt(parts[2])));
+          break;
+        case "multiply":
+          System.out.println(calculator.multiply(Integer.parseInt(parts[1]), Integer.parseInt(parts[2])));
+          break;
+        case "divide":
+          System.out.println(calculator.divide(Integer.parseInt(parts[1]), Integer.parseInt(parts[2])));
+          break;
+        case "fibonacci":
+          System.out.println(calculator.fibonacciNumberFinder(Integer.parseInt(parts[1])));
+          break;
+        case "binary":
+          System.out.println(calculator.intToBinaryNumber(Integer.parseInt(parts[1])));
+          break;
+        default:
+          System.out.println("Invalid command!");
+          break;
+      }
     }
   }
 }
